c56a20c6ee83414c9b4d52f1b0ed6a7d5d446828
Nam Nguyen <namhngu@uw.edu>
2022-10-23 21:10:41 -0700

    jiojijo

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+const io = require('socket.io')();
+
+io.on('connection', client => {
+    client.emit('init', {data: 'hello world'})
+})
+
---
+++ frontend/index.js
@@ -0,0 +1,51 @@
+const WHITE = '#000000';
+const size = 30;
+const gameScreen = document.getElementById('gameScreen');
+const colorInput = document.getElementById('colorInput');
+const gameSize = 600;
+const canva = document.getElementById('canvas');
+
+let canvas, ctx;
+
+var x;
+var y;
+var paints;
+
+function init() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    screenBounds = canva.getBoundingClientRect();
+
+    canvas.width = canvas.height = gameSize;
+    ctx.fillStyle = WHITE;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    document.addEventListener('mousedown', mousedown);
+    paints = 5;
+}
+
+function mousedown(e) {
+    screenBounds = canva.getBoundingClientRect();
+    x = Math.floor((e.clientX - screenBounds.left)/size);
+    y = Math.floor((e.clientY - screenBounds.top)/size);
+    color = colorInput.value;
+    if (paints > 0  && 0 <= x && x <= (gameSize/size) && 0 <= y && y <= (gameSize/size) && isColor(color)){
+        console.log(color);
+        paintPixel(x,y, color);
+        paints--;
+    } else {
+        console.log("Wrong color");
+    }
+}
+
+init();
+
+function paintPixel(x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x * size, y * size, size, size);
+}
+
+function isColor(color){
+    var s = new Option().style;
+    s.color = color;
+    return s.color !== '';
+  }
